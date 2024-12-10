8ca6c62ae30237a283ea7bd4cd2792f2b61fc0bc
pplancq <paul.plancq@outlook.fr>
2024-12-10 19:33:34 +0100

    ci(svg-react): update ci for testing with react 18 and 19

--- packages/svg-react/vitest.setup.ts
+++ packages/svg-react/vitest.setup.ts
@@ -1,8 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom/vitest';
 import { cleanup } from '@testing-library/react';
+import { version } from 'react';
 import { afterEach } from 'vitest';
 
 afterEach(() => {
   cleanup();
 });
+
+console.info(`vitest has been loaded with react v${version}`);
