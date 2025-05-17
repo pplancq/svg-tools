f98548d3e601feec0befac0929e40f1c120dc86b
pplancq <paul.plancq@outlook.fr>
2025-05-17 16:11:21 +0200

    docs(storybook): add documentation and stories for Svg component

---
+++ apps/storybook/stories/Svg.stories.tsx
@@ -0,0 +1,45 @@
+import appleIcon from '@pplancq/shelter-ui-icon/logo/apple-original.svg?url';
+import reactIcon from '@pplancq/shelter-ui-icon/logo/react-original.svg?inline';
+import { Svg } from '@pplancq/svg-react';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+const icons: Record<string, string> = {
+  'apple url': appleIcon,
+  'react inline': reactIcon,
+  'simulate svg error': 'foo',
+};
+
+const meta: Meta<typeof Svg> = {
+  title: 'Package/svg-react',
+  component: Svg,
+  parameters: {
+    controls: { expanded: true },
+  },
+  args: {
+    src: 'apple url',
+    alt: 'svg description',
+    width: 48,
+    height: 48,
+    fill: '',
+  },
+  argTypes: {
+    src: {
+      options: Object.keys(icons),
+      control: { type: 'select' },
+      description: 'The source of the SVG icon. It can be a URL or an inline SVG string.',
+    },
+    alt: {
+      control: { type: 'text' },
+      description: 'Alternative text if the SVG image cannot be loaded (accessibility).',
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Svg>;
+
+export const Playground: Story = {
+  render: ({ src, ...args }) => <Svg src={icons[src]} {...args} />,
+  tags: ['!dev'],
+};
