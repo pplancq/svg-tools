993a7ff901feb24704e9a9f95d7b477607e8a3a8
pplancq <paul.plancq@outlook.fr>
2025-05-17 15:24:31 +0200

    chore(storybook): add custom theme and use shelter-ui

--- apps/storybook/.storybook/preview.ts
+++ apps/storybook/.storybook/preview.ts
@@ -1,12 +1,18 @@
 import type { Preview } from '@storybook/react-vite';
+import { light } from './theme';
+
+import '@pplancq/shelter-ui-css/css/shelter-ui.css';
 
 const preview: Preview = {
   parameters: {
+    backgrounds: { disable: true },
     docs: {
+      theme: light,
       toc: {
         title: 'On this page',
         headingSelector: 'h2, h3',
       },
+      codePanel: true,
     },
     controls: {
       matchers: {
@@ -20,7 +26,7 @@ const preview: Preview = {
       },
     },
   },
-  tags: ['autodocs'],
+  tags: ['!autodocs'],
 };
 
 export default preview;
