08cf3254ae439a39fc0fcf4e6dd648de4b6885f8
pplancq <paul.plancq@outlook.fr>
2025-05-27 19:37:05 +0200

    feat(svg-core)!: introduce custom error classes for SVG validation
    BREAKING CHANGE: the getSvg function now throws custom exceptions (InvalidSvgError, ContentSvgError) in case of errors when retrieving or validating an SVG.

--- packages/svg-core/src/getSvg.ts
+++ packages/svg-core/src/getSvg.ts
@@ -3,31 +3,38 @@ import { MINE_TYPE_SVG } from './constants';
 import { fetchSvg } from './fetchSvg';
 import { mergeAttributes } from './mergeAttributes';
 
-export const getSvg = async (path: string | URL, svgElement?: SVGSVGElement): Promise<SVGSVGElement | null> => {
+/**
+ * Retrieves an SVG element from a given path or URL, sanitizes it, and merges its attributes with an optional SVG element.
+ *
+ * @param {string | URL} path - The path or URL to the SVG file or data URI.
+ * @param {SVGSVGElement} [svgElement] - An optional SVG element to merge attributes into.
+ *
+ * @returns {Promise<SVGSVGElement>} - A promise that resolves to the SVG element.
+ *
+ * @throws {InvalidSvgError} - If the fetched content is not a valid SVG.
+ * @throws {ContentSvgError} - If the content of the SVG file is not valid.
+ */
+export const getSvg = async (path: string | URL, svgElement?: SVGSVGElement): Promise<SVGSVGElement> => {
   const svgEl = svgElement || document.createElementNS('http://www.w3.org/2000/svg', 'svg');
 
-  try {
-    let svgData: string;
-    if (typeof path === 'string' && path.startsWith(`data:${MINE_TYPE_SVG}`)) {
-      const svgEncoded = path.substring(path.indexOf(',') + 1);
-      svgData = path.includes('base64') ? atob(svgEncoded) : decodeURIComponent(svgEncoded);
-    } else {
-      svgData = await fetchSvg(path);
-    }
-
-    const parent = document.createElement('div');
-    parent.innerHTML = DOMPurify.sanitize(svgData, {
-      USE_PROFILES: { svg: true, svgFilters: true },
-      IN_PLACE: true,
-    });
-
-    const svg = parent.firstChild as SVGSVGElement;
-
-    mergeAttributes(svg, svgEl);
-    svgEl.innerHTML = svg.innerHTML;
-
-    return svgEl;
-  } catch {
-    return null;
+  let svgData: string;
+  if (typeof path === 'string' && path.startsWith(`data:${MINE_TYPE_SVG}`)) {
+    const svgEncoded = path.substring(path.indexOf(',') + 1);
+    svgData = path.includes('base64') ? atob(svgEncoded) : decodeURIComponent(svgEncoded);
+  } else {
+    svgData = await fetchSvg(path);
   }
+
+  const parent = document.createElement('div');
+  parent.innerHTML = DOMPurify.sanitize(svgData, {
+    USE_PROFILES: { svg: true, svgFilters: true },
+    IN_PLACE: true,
+  });
+
+  const svg = parent.firstChild as SVGSVGElement;
+
+  mergeAttributes(svg, svgEl);
+  svgEl.innerHTML = svg.innerHTML;
+
+  return svgEl;
 };
