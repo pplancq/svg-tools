01f713b80eaf79df08829902f022f35a42e78595
pplancq <paul.plancq@outlook.fr>
2025-06-16 18:20:01 +0200

    fix(svg-react): import act form @testing-library/react

--- packages/svg-react/src/helper/tests/renderSuspense.ts
+++ packages/svg-react/src/helper/tests/renderSuspense.ts
@@ -1,6 +1,5 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { render, type RenderOptions } from '@testing-library/react';
-import { act, type ReactNode } from 'react';
+import { act, render, type RenderOptions } from '@testing-library/react';
+import { type ReactNode } from 'react';
 
 export const renderSuspense = (ui: ReactNode, options?: Omit<RenderOptions, 'queries'> | undefined) =>
   act(async () => {
