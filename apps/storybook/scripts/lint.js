9ee2a30a24ab3f8495b48b7445aba861c118c485
Paul PLANCQ <paul.plancq@outlook.fr>
2026-06-04 10:39:11 +0200

    chore(svg-tools): update concurrently to v10.0.3 and fix linting rule

--- apps/storybook/scripts/lint.js
+++ apps/storybook/scripts/lint.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+// eslint-disable-next-line import/no-unresolved
 import concurrently from 'concurrently';
 
 const { result } = concurrently(['npm:lint:*(!fix)'], {
